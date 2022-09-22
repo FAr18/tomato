94cd4a975d2b63aff4dbb25713afaca911c37abc
JinFa <qazwedcx5t@gmail.com>
2022-09-22 20:45:31 +0800

    feat: allow to show menu open state

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <TomatoBackground />
+  <TomatoBackground :is-menu-opened="isMenuOpened" />
   <TomatoLogo />
-  <TomatoClock />
+  <TomatoClock :is-menu-opened="isMenuOpened" />
 </template>
 
 <script setup>
 import TomatoLogo from "./components/TomatoLogo.vue";
 import TomatoBackground from "./components/TomatoBackground.vue";
 import TomatoClock from "./components/TomatoClock.vue";
+import { ref, onMounted } from "vue";
+const isMenuOpened = ref(false);
 </script>
 
 <style lang="scss" scoped></style>
--- src/components/TomatoClock.vue
+++ src/components/TomatoClock.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="clock-container" ref="clockContainer">
-    <CircleProgressBar :size="progressSize" :progress="progressValue" />
+  <div
+    class="clock-container"
+    :class="{ 'is-menu-opened': isMenuOpened }"
+    ref="clockContainer"
+  >
+    <CircleProgressBar :size="progressSize" :progress="progressValue" :stroke-width="5" />
   </div>
 </template>
 
@@ -11,6 +15,13 @@ const progressValue = ref(0);
 const progressSize = ref(0);
 
 const clockContainer = ref(null);
+
+const props = defineProps({
+  isMenuOpened: {
+    type: Boolean,
+    default: false,
+  },
+});
 
 // const increaseProgress = () => {
 //   progressValue.value = Math.min(progressValue.value + 1, 100);
@@ -28,6 +39,7 @@ onMounted(() => {
   window.addEventListener("resize", () => {
     progressSize.value = clockContainer.value.clientHeight;
   });
+  // test();
 });
 </script>
 
@@ -39,5 +51,9 @@ onMounted(() => {
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+  transition: left 1s;
+  &.is-menu-opened {
+    left: 25%;
+  }
 }
 </style>
